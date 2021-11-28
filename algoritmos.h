ddddd9688722463167f4751f1c55950540b28bf2
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2021-11-27 22:09:08 -0300

    partes desnecessarias

--- algoritmos.h
+++ algoritmos.h
@@ -1,10 +1,8 @@
 // BubbleSort
 void bubbleSort(int *array, int tamanhoDoarray);
-void troca(int *a, int *b);
 
 // Insertion Sort
 void insertionSort(int array[], int tamanhoDoarray);
 
 // Merge Sort
-void merge(int array[], int comeco, int meio, int fim);
 void mergeSort(int array[], int comeco, int fim);
